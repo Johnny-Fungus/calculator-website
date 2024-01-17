b41f2cc96ec5b647bd7eab07d81838d4a17843ee
Johnny-Fungus <150959957+Johnny-Fungus@users.noreply.github.com>
2024-01-16 21:16:09 -0600

    made changes

--- actions.js
+++ actions.js
@@ -1,7 +1,7 @@
 function add(){
     num1 = Number(document.getElementById("first entry box").value)
     num2 = Number(document.getElementById("second entry box").value)
-    document.getElementById("answer").innerHTML(num1 + num2)
+    document.getElementById("answer").innerHTML = num1 + num2
 }
 
 function subtract(){
