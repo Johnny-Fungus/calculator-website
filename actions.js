17f8adca1dce837a5f996534fe92af99aad78be4
Johnny-Fungus <150959957+Johnny-Fungus@users.noreply.github.com>
2024-01-16 21:05:35 -0600

    adding files
    adding files

---
+++ actions.js
@@ -0,0 +1,17 @@
+function add(){
+    num1 = Number(document.getElementById("first entry box").value)
+    num2 = Number(document.getElementById("second entry box").value)
+    document.getElementById("answer").innerHTML(num1 + num2)
+}
+
+function subtract(){
+    alert("subtract")
+}
+
+function divide(){
+    alert("divide")
+}
+
+function multiply(){
+    alert("multiply")
+}
